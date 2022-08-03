e4a4cec879dd7ae0eaa33a7991ff0f93fd9ad9ef
Han <yinglinhan1987@gmail.com>
2022-08-03 17:46:22 +0800

    更新第十五章

---
+++ chapter15/05 服务器的工作机制/lesson/main.js
@@ -0,0 +1,3 @@
+// apache  nginx  
+
+// webServer
---
+++ chapter15/08 请求报文和响应报文/lesson/main.js
@@ -0,0 +1,16 @@
+
+
+/* 
+MIME type
+
+文本：text/plain、text/html、text/css、text/javascript、text/xml
+
+图片：image/gif、image/png、image/jpeg
+
+视频：video/webm、video/ogg
+
+音频：audio/midi、audio/mpeg、audio/webm、audio/ogg、audio/wav
+
+二进制：application/octet-stream、application/pdf、application/json
+
+*/
